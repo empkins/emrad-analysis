77dd9270bafbc4dfad32fa0a335576d6773692d1
Simon Meske <simon.h.meske@fau.de>
2024-04-17 21:44:15 +0200

    Changed to environ variable

--- main.py
+++ main.py
@@ -3,11 +3,14 @@ from pathlib import Path
 from rbm_robust.data_loading.datasets import D02Dataset
 from rbm_robust.pipelines.cnnLstmPipeline import CnnPipeline
 from rbm_robust.validation.scoring import cnnPipelineScoring
+import os
 
 
 def main():
     print("Starting")
-    # dataset_path = Path("/Users/simonmeske/Desktop/TestOrdner/data_per_subject")
+    path = os.environ.get("DATA_PATH")
+    print(path)
+    # dataset_path = Path(path)
     # dataset = D02Dataset(dataset_path)
     # cnn_pipeline = CnnPipeline()
     # cnnPipelineScoring(cnn_pipeline, dataset)
