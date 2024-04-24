44189ae6a00d73b23974a97056f51a3f5833f64d
Simon Meske <simon.h.meske@fau.de>
2024-04-24 22:31:32 +0200

    Refactoring and Model change - But Problems with the Model :/

--- rbm_robust/models/cnn.py
+++ rbm_robust/models/cnn.py
@@ -1,5 +1,6 @@
 import os
 from datetime import datetime
+from pathlib import Path
 from typing import Optional, Tuple
 import tensorflow as tf
 import pickle
@@ -66,183 +67,85 @@ class CNN(Algorithm):
         self._model = _model
 
     def batch_generator_images(self, base_path):
-        subjects = [name for name in os.listdir(base_path) if os.path.isdir(os.path.join(base_path, name))]
-        i = 0
-        while i < len(subjects):
-            if i == len(subjects):
-                i = 0
-            subject_id = subjects[i]
-            subject_path = os.path.join(base_path, subject_id)
-            phases = [name for name in os.listdir(subject_path) if os.path.isdir(os.path.join(subject_path, name))]
+        base_path = Path(base_path)
+        subjects = [path.name for path in base_path.iterdir() if path.is_dir()]
+        for subject_id in subjects:
+            subject_path = base_path / subject_id
+            phases = [path.name for path in subject_path.iterdir() if path.is_dir()]
             for phase in phases:
-                phase_path = os.path.join(subject_path, phase)
-                input_path = os.path.join(phase_path, "inputs")
-                label_path = os.path.join(phase_path, "labels")
-                if not os.path.exists(input_path) or not os.path.exists(label_path):
+                phase_path = subject_path / phase
+                input_path = phase_path / "inputs"
+                label_path = phase_path / "labels"
+                if not input_path.exists() or not label_path.exists():
                     continue
-                input_names = [
-                    name for name in os.listdir(input_path) if os.path.isfile(os.path.join(input_path, name))
-                ]
-                input_names.sort()
+                input_names = sorted(path.name for path in input_path.iterdir() if path.is_file())
                 grouped_inputs = {k: list(g) for k, g in groupby(input_names, key=lambda s: s.split("_")[0])}
-                sorted_keys = sorted(grouped_inputs.keys())
-                for key in sorted_keys:
-                    label = np.load(os.path.join(label_path, f"{key}.npy"))
-                    if ".png" in grouped_inputs[key][0]:
-                        inputs = [
-                            img_to_array(load_img(os.path.join(input_path, f"{in_path}"), target_size=(255, 1000)))
-                            for in_path in grouped_inputs[key]
-                        ]
-                    elif ".npy" in grouped_inputs[key][0]:
-                        inputs = [np.load(os.path.join(input_path, f"{in_path}")) for in_path in grouped_inputs[key]]
+                for key, group in grouped_inputs.items():
+                    label = np.load(label_path / f"{key}.npy")
+                    inputs = [self._load_input(input_path / name) for name in group]
+                    inputs = np.stack(inputs, axis=0)
+                    inputs = np.transpose(inputs)
+                    inputs = np.array([inputs])
                     yield inputs, label
 
-    def batch_generator(self, base_path):
-        subjects = [name for name in os.listdir(base_path) if os.path.isdir(os.path.join(base_path, name))]
-        i = 0
-        batch_size = self.batch_size
-        while True:
-            if i == len(subjects):
-                i = 0  # reset the counter if we've gone through all subjects
-            try:
-                subject_id = subjects[i]
-            except Exception as _:
-                print(f"i is {i}")
-                print(subjects)
-                raise ValueError("oh no")
-            phases = [
-                name
-                for name in os.listdir(os.path.join(base_path, subject_id))
-                if os.path.isdir(os.path.join(base_path, subject_id, name))
-            ]
-
-            for phase in phases:
-                phase_path = os.path.join(base_path, subject_id, phase)
-                input_path = os.path.join(phase_path, "inputs")
-                label_path = os.path.join(phase_path, "labels")
-
-                if not os.path.exists(input_path) or not os.path.exists(label_path):
-                    continue
-
-                input_paths = [
-                    os.path.join(input_path, file) for file in os.listdir(input_path) if file.endswith(".pkl")
-                ]
-                label_paths = [
-                    os.path.join(label_path, file) for file in os.listdir(label_path) if file.endswith(".pkl")
-                ]
-
-                # Order the paths
-                input_paths.sort()
-                label_paths.sort()
-
-                # zip the paths
-                paths = zip(input_paths, label_paths)
-
-                for element in paths:
-                    # Load inputs
-                    with open(element[0], "rb") as f:
-                        inputs = pickle.load(f)
-                    # Load labels
-                    with open(element[1], "rb") as w:
-                        labels = pickle.load(w)
-
-                    ins = np.stack(inputs, axis=0)
-                    labs = np.stack(labels, axis=0)
-                    labs = np.transpose(labs)
-                    # print(f"labels len {len(labels)}")
-                    # print(f"labs shape {labs.shape}")
-                    for j in range(ins.shape[0]):
-                        sub_ins = ins[j : j + 1, :, :, :]
-                        sub_lab = labs[:, j : j + 1]
-                        # print(f"input shape: {sub_ins.shape} sub_lab shape: {sub_lab.shape}")
-                        yield sub_ins, sub_lab
-            i += 1
+    def _load_input(self, path):
+        if path.suffix == ".png":
+            return img_to_array(load_img(path, target_size=(255, 1000)))
+        elif path.suffix == ".npy":
+            return np.load(path)
 
     def get_steps_per_epoch(self, base_path):
+        base_path = Path(base_path)
         steps = 0
-        subjects = [name for name in os.listdir(base_path) if os.path.isdir(os.path.join(base_path, name))]
-        for subject_id in subjects:
-            phases = [
-                name
-                for name in os.listdir(os.path.join(base_path, subject_id))
-                if os.path.isdir(os.path.join(base_path, subject_id, name))
-            ]
-
-            for phase in phases:
-                phase_path = os.path.join(base_path, subject_id, phase)
-                input_path = os.path.join(phase_path, "inputs")
-                label_path = os.path.join(phase_path, "labels")
-
-                if not os.path.exists(input_path) or not os.path.exists(label_path):
+        for subject_path in base_path.iterdir():
+            if not subject_path.is_dir():
+                continue
+            for phase_path in subject_path.iterdir():
+                if not phase_path.is_dir():
                     continue
-
-                input_paths = [
-                    os.path.join(input_path, file) for file in os.listdir(input_path) if file.endswith(".pkl")
-                ]
-                for element in input_paths:
-                    # Load inputs
-                    with open(element, "rb") as f:
-                        inputs = pickle.load(f)
-                    steps += len(inputs)
+                input_path = phase_path / "inputs"
+                label_path = phase_path / "labels"
+                if not input_path.exists() or not label_path.exists():
+                    continue
+                input_names = sorted(path.name for path in input_path.iterdir() if path.is_file())
+                grouped_inputs = {k: list(g) for k, g in groupby(input_names, key=lambda s: s.split("_")[0])}
+                steps += len(grouped_inputs.keys())
         return steps
 
     def predict(self, data_path: str):
-        subjects = [name for name in os.listdir(data_path) if os.path.isdir(os.path.join(data_path, name))]
-        predictions = {}
-
-        for subject_id in subjects:
-            subject_dict = {}
-            subject_path = os.path.join(data_path, subject_id)
-            phases = [name for name in os.listdir(subject_path) if os.path.isdir(os.path.join(subject_path, name))]
-            phase_predictions = []
-            for phase in phases:
-                phase_path = os.path.join(data_path, subject_id, phase)
-                prediction_path = os.path.join(phase_path, "predictions")
-                if not os.path.exists(prediction_path):
-                    os.makedirs(prediction_path)
-                input_path = os.path.join(phase_path, "inputs")
-                input_paths = [
-                    os.path.join(input_path, file) for file in os.listdir(input_path) if file.endswith(".pkl")
-                ]
-                input_paths.sort()
-                for element in input_paths:
-                    with open(element, "rb") as f:
-                        inputs = pickle.load(f)
-                    ins = np.stack(inputs, axis=0)
-                    for j in range(ins.shape[0]):
-                        sub_ins = ins[j : j + 1, :, :, :]
-                        pred = self._model.predict(sub_ins)
-                        print(f"Predictions for {ins.shape} are {pred.shape} shape")
-                        phase_predictions.append(self._model.predict(sub_ins))
-                subject_dict[phase] = phase_predictions
-            predictions[subject_id] = subject_dict
-        self.predictions_ = predictions
+        data_path = Path(data_path)
+        for subject_path in data_path.iterdir():
+            if not subject_path.is_dir():
+                continue
+            for phase_path in subject_path.iterdir():
+                if not phase_path.is_dir():
+                    continue
+                input_path = phase_path / "inputs"
+                prediction_path = phase_path / "predictions"
+                prediction_path.mkdir(exist_ok=True)
+                input_files = sorted(input_path.glob("*.pkl"))
+                grouped_inputs = {k: list(g) for k, g in groupby(input_files, key=lambda s: s.stem.split("_")[0])}
+                for key, group in grouped_inputs.items():
+                    inputs = [np.load(file) for file in group]
+                    pred = self._model.predict(inputs)
+                    np.save(prediction_path / f"{key}.npy", pred)
+                    print(f"Predictions for {inputs.shape} are {pred.shape} shape")
         return self
 
-    def self_optimize(self, training_data_path: str, label_path: str):
-        """Use the training data and the corresponding labels to train the model with the hyperparameters passed in the init
-
-        Args:
-            training_data (list): training data, multiple inputs
-            labels (np.ndarray): corresponding labels
-        """
-
+    def self_optimize(self, training_data_path: str, base_path: str = "Data"):
         if self._model is None:
             self._create_model()
 
-        log_dir = "Runs/logs/fit/" + datetime.now().strftime("%Y%m%d-%H%M%S")
+        log_dir = "Runs/logs/fit/"
+        log_dir += datetime.now().strftime("%Y%m%d-%H%M%S")
+        if not os.path.exists(log_dir):
+            os.makedirs(log_dir)
+
         tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
 
-        batch_generator = self.batch_generator_images("Data")
-        steps = self.get_steps_per_epoch("Data")
+        batch_generator = self.batch_generator_images(base_path)
+        steps = self.get_steps_per_epoch(base_path)
 
-        # assert (
-        #     self._model.layers[0].input_shape[1] == training_data.shape[1]
-        # ), f"Your training data has dimension {training_data.shape} while the model has input shape {self._model.layers[0].input_shape}!"
-        # assert (
-        #     self._model.layers[0].input_shape[2] == training_data.shape[2]
-        # ), f"Your training data has dimension {training_data.shape} while the model has input shape {self._model.layers[0].input_shape}!"
-        print(f"Is compiled {self._model._is_compiled}")
         print("Fitting")
         self._model.fit(
             batch_generator,
@@ -257,8 +160,14 @@ class CNN(Algorithm):
 
     def _create_model(self):
         self._model = keras.Sequential()
-        self._model.add(keras.layers.Conv2D(3, (1, 1), padding="same", input_shape=(255, 1000, 5)))
-        self._model.add(keras.applications.ResNet50V2(include_top=False, weights="imagenet", pooling="avg"))
+        self._model.add(
+            keras.layers.Conv2D(3, (1, 1), padding="same", input_shape=(1, 1000, 255, 5), batch_size=self.batch_size)
+        )
+        self._model.add(
+            keras.layers.TimeDistributed(
+                keras.applications.ResNet50V2(include_top=False, weights="imagenet", input_shape=(1000, 255, 3))
+            )
+        )
         self._model.add(keras.layers.Flatten())
         self._model.add(keras.layers.Dense(1000))
         self._model.compile(optimizer="adam", loss="mse")
