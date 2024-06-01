b50ad7336e4531cf17548d16b7fa3fcd5332f4aa
Simon Meske <simon.h.meske@fau.de>
2024-06-01 12:13:54 +0200

    Removed KL since the y_true is no prob. dist.

--- rbm_robust/models/cnn.py
+++ rbm_robust/models/cnn.py
@@ -249,8 +249,8 @@ class CNN(Algorithm):
         # self._model.add(layers.TimeDistributed(layers.Flatten()))
         # self._model.add(layers.TimeDistributed(layers.Dense(units=1)))
         self._model.add(layers.Conv2D(filters=1, kernel_size=(1, 256), activation="linear"))
-        # loss_func = keras.losses.BinaryCrossentropy(from_logits=False, reduction="none")
-        self._model.compile(optimizer="adam", loss=keras.losses.kl_divergence)
+        loss_func = keras.losses.BinaryCrossentropy(from_logits=False, reduction="none")
+        self._model.compile(optimizer="adam", loss=loss_func)
         # self._model.compile(optimizer="adam", loss="mse")
         return self
 
