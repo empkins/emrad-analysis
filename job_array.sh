c9cd7fa3a7c4f0b208ec779dd922946ffccc2026
Simon Meske <simon.h.meske@fau.de>
2024-06-16 14:40:35 +0200

    Changed job scripts

--- job_array.sh
+++ job_array.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -l
 #
-#SBATCH --job-name=uNetRadarcadia300
+#SBATCH --job-name=uNetRadarcadiaArray50
 #SBATCH --nodes=1
 #SBATCH --time=7:30:00
 #SBATCH --gres=gpu:1
@@ -15,11 +15,11 @@ module add tensorrt/8.6.1.6-cuda12.0-cudnn8.9
 rsync -r $HPCVAULT/DataRadarcadia $TMPDIR/Data
 
 cd "$HOME"/emrad-analysis || exit
-poetry run python main.py --epochs 30 --learning_rate 0.0001 --image_based False --datasource radarcadia --breathing_type all --label_type ecg --dual_channel False --log False
-poetry run python main.py --epochs 30 --learning_rate 0.0001 --image_based False --datasource radarcadia --breathing_type all --label_type ecg --dual_channel False --log True
-poetry run python main.py --epochs 30 --learning_rate 0.0001 --image_based False --datasource radarcadia --breathing_type all --label_type ecg --dual_channel True
+poetry run python main.py --epochs 50 --learning_rate 0.0001 --image_based False --datasource radarcadia --breathing_type all --label_type ecg --dual_channel False --log False
+poetry run python main.py --epochs 50 --learning_rate 0.0001 --image_based False --datasource radarcadia --breathing_type all --label_type ecg --dual_channel False --log True
+poetry run python main.py --epochs 50 --learning_rate 0.0001 --image_based False --datasource radarcadia --breathing_type all --label_type ecg --dual_channel True
 
 
-poetry run python main.py --epochs 30 --learning_rate 0.0001 --image_based False --datasource radarcadia --breathing_type all --label_type gaussian --dual_channel False --log False
-poetry run python main.py --epochs 30 --learning_rate 0.0001 --image_based False --datasource radarcadia --breathing_type all --label_type gaussian --dual_channel False --log True
-poetry run python main.py --epochs 30 --learning_rate 0.0001 --image_based False --datasource radarcadia --breathing_type all --label_type gaussian --dual_channel True
+poetry run python main.py --epochs 50 --learning_rate 0.0001 --image_based False --datasource radarcadia --breathing_type all --label_type gaussian --dual_channel False --log False
+poetry run python main.py --epochs 50 --learning_rate 0.0001 --image_based False --datasource radarcadia --breathing_type all --label_type gaussian --dual_channel False --log True
+poetry run python main.py --epochs 50 --learning_rate 0.0001 --image_based False --datasource radarcadia --breathing_type all --label_type gaussian --dual_channel True
