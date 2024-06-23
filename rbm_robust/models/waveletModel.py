b3f4a1e153a66b3837edf8a8060e2651c5c3897b
Simon Meske <simon.h.meske@fau.de>
2024-06-24 00:30:20 +0200

    Processing D02

--- rbm_robust/models/waveletModel.py
+++ rbm_robust/models/waveletModel.py
@@ -59,11 +59,11 @@ class UNetWaveletTF(Algorithm):
         self,
     ):
         self._create_model()
-        # log_dir = os.getenv("HPCVAULT") + f"/Runs/logs/fit/{self.model_name}"
-        # if not os.path.exists(log_dir):
-        #     os.makedirs(log_dir)
-        #
-        # tensorboard_callback = keras.callbacks.TensorBoard(log_dir=log_dir, write_graph=False, update_freq="epoch")
+        log_dir = os.getenv("HPCVAULT") + f"/Runs/logs/fit/{self.model_name}"
+        if not os.path.exists(log_dir):
+            os.makedirs(log_dir)
+
+        tensorboard_callback = keras.callbacks.TensorBoard(log_dir=log_dir, write_graph=False, update_freq="epoch")
         print("Fitting")
         history = self._model.fit(
             self.training_ds,
@@ -74,14 +74,14 @@ class UNetWaveletTF(Algorithm):
             validation_data=self.validation_ds,
             validation_steps=self.validation_steps,
             verbose=1,
-            # callbacks=[tensorboard_callback],
+            callbacks=[tensorboard_callback],
         )
 
-        # history_path = os.getenv("HPCVAULT") + "/Runs/History/"
-        # if not os.path.exists(history_path):
-        #     os.makedirs(history_path)
-        # history_path += self.model_name + "_history.pkl"
-        # pickle.dump(history.history, open(history_path, "wb"))
+        history_path = os.getenv("HPCVAULT") + "/Runs/History/"
+        if not os.path.exists(history_path):
+            os.makedirs(history_path)
+        history_path += self.model_name + "_history.pkl"
+        pickle.dump(history.history, open(history_path, "wb"))
         self.save_model()
         return self
 
