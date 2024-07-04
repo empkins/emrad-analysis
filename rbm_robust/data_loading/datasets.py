8236be20c0d2091d2aca1b7b9a54cff30a214bfa
Simon Meske <simon.h.meske@fau.de>
2024-07-04 08:37:21 +0200

    Preprocess fix

--- rbm_robust/data_loading/datasets.py
+++ rbm_robust/data_loading/datasets.py
@@ -359,7 +359,7 @@ class D02Dataset(Dataset):
         # participant_ids = [pid for pid in participant_ids if pid in self.REDUCED_SUBEJCTS]
         # participant_ids = [pid for pid in participant_ids if pid in self.TEST_SUBJECTS]
 
-        participant_ids = [pid for pid in participant_ids if int(pid) % 2 == 0]
+        participant_ids = [pid for pid in participant_ids if int(pid) % 2 != 0]
         # participant_ids = ["240"]
 
         # PROC = [item.name for item in Path("/home/woody/iwso/iwso116h/DataD02").iterdir() if item.is_dir()]
