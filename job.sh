0ce83282d8e3f164e28c0657ac7975cfbed77c1e
Simon Meske <simon.h.meske@fau.de>
2024-07-04 18:44:55 +0200

    Preprocess fix

--- job.sh
+++ job.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -l
 #
-#SBATCH --job-name=uNetRadarcadia300
+#SBATCH --job-name=uNetD02Morl
 #SBATCH --nodes=1
-#SBATCH --time=7:30:00
+#SBATCH --time=19:30:00
 #SBATCH --gres=gpu:1
 
 
@@ -12,21 +12,8 @@ module load python/3.10-anaconda
 export OUTDATED_IGNORE=1
 export PATH="/home/hpc/iwso/iwso116h/.local/bin:$PATH"
 module add tensorrt/8.6.1.6-cuda12.0-cudnn8.9
-rsync -r $HPCVAULT/DataRadarcadia $TMPDIR/Data
+rsync -r $WORK/DataD02 $TMPDIR
 
 cd "$HOME"/emrad-analysis || exit
 
-poetry run python main.py --epochs 30 --learning_rate 0.0001 --image_based False --datasource d02 --log False --label_type gaussian --dual_channel False --wavelet morl --identity False --diff True
-
-
-
-poetry run python main.py --epochs 30 --learning_rate 0.0001 --image_based False --datasource radarcadia --breathing_type all --label_type ecg --dual_channel True
-poetry run python main.py --epochs 30 --learning_rate 0.0001 --image_based False --datasource radarcadia --breathing_type all --label_type gaussian --dual_channel True
-poetry run python main.py --epochs 30 --learning_rate 0.0001 --image_based False --datasource radarcadia --breathing_type all --label_type ecg --dual_channel False --log True
-poetry run python main.py --epochs 30 --learning_rate 0.0001 --image_based False --datasource radarcadia --breathing_type all --label_type ecg --dual_channel False --log False
-
-poetry run python main.py --epochs 30 --learning_rate 0.0001 --image_based True --datasource radarcadia --breathing_type all --label_type ecg --dual_channel False --log False
-poetry run python main.py --epochs 30 --learning_rate 0.0001 --image_based True --datasource radarcadia --breathing_type all --label_type ecg --dual_channel False --log True
-poetry run python main.py --epochs 30 --learning_rate 0.0001 --image_based True --datasource radarcadia --breathing_type all --label_type gaussian --dual_channel False --log False
-poetry run python main.py --epochs 30 --learning_rate 0.0001 --image_based True --datasource radarcadia --breathing_type all --label_type gaussian --dual_channel False --log True
-
+poetry run python main.py --epochs 30 --learning_rate 0.0001 --image_based False --datasource d02 --log False --label_type gaussian --dual_channel False --wavelet morl --identity False
