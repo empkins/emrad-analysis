0b939a537ed88f064c3840d97643492076b35332
Simon Meske <simon.h.meske@fau.de>
2024-07-15 07:54:44 +0200

    Test with mulitresnet 3plus

--- main.py
+++ main.py
@@ -714,10 +714,10 @@ def collect_and_score_arrays_radarcadia():
 if __name__ == "__main__":
     # collect_and_score_arrays_d02()
     # collect_and_score_arrays_radarcadia()
-    main()
+    # main()
     # scoring()
-    # pretrained(os.getenv("HOME") + "/emrad-analysis/Models")
-    # pretrained(os.getenv("HOME") + "/altPreprocessing/emrad-analysis/Models")
+    pretrained(os.getenv("HOME") + "/emrad-analysis/Models")
+    pretrained(os.getenv("HOME") + "/altPreprocessing/emrad-analysis/Models")
     # preprocessing_magnitude(dataset="radarcadia")
     # move_training_data()
     # preprocessing()
