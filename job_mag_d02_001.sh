a5bdc128ad01535375fa61c4cbe081ab78d14a7a
Simon Meske <simon.h.meske@fau.de>
2024-07-27 12:15:42 +0200

    Test with multunet 3plus

--- job_mag_d02_001.sh
+++ job_mag_d02_001.sh
@@ -11,7 +11,7 @@ module load python/3.10-anaconda
 export OUTDATED_IGNORE=1
 export PATH="/home/hpc/iwso/iwso116h/.local/bin:$PATH"
 module add tensorrt/8.6.1.6-cuda12.0-cudnn8.9
-rsync -r $HPCVAULT/DataD02 $TMPDIR/Data
+rsync -r $WORK/DataD02 $TMPDIR/Data
 
 cd "$HOME"/altPreprocessing/emrad-analysis || exit
 poetry run python main.py --epochs 350 --learning_rate 0.001 --image_based False --datasource d02 --label_type gaussian --wavelet mag --mag True
