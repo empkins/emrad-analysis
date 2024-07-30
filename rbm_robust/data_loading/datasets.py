af49a067a6126c30e63051063ac823e988d10da2
Simon Meske <simon.h.meske@fau.de>
2024-07-30 21:58:19 +0200

    Test with multunet 3plus

--- rbm_robust/data_loading/datasets.py
+++ rbm_robust/data_loading/datasets.py
@@ -142,9 +142,11 @@ class D02Dataset(Dataset):
         participant_ids = [item.name for item in Path(self.data_path).iterdir() if item.is_dir()]
         participant_ids = [pid for pid in participant_ids if pid not in self.EXCLUDE_SUBJECTS]
 
-        # ALR = [item.name for item in Path("/home/woody/iwso/iwso116h/DataD02EMD").iterdir() if item.is_dir()]
+        ALR = [item.name for item in Path("/home/woody/iwso/iwso116h/DataD02").iterdir() if item.is_dir()]
         PROC = ["007", "316", "338"]
         participant_ids = [pid for pid in participant_ids if pid not in PROC]
+        AGAIN = ["308", "120", "287", "143"]
+        participant_ids = participant_ids + AGAIN
         # participant_ids = [pid for pid in participant_ids if pid not in ALR]
         df = pd.DataFrame({"participant": participant_ids})
         if df.empty:
