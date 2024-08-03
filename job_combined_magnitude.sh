2fbdf5d150d356d9a0f7f7b1dbed5df05051f913
Simon Meske <simon.h.meske@fau.de>
2024-08-03 17:41:20 +0200

    Test with multunet 3plus

--- job_combined_magnitude.sh
+++ job_combined_magnitude.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -l
 #
-#SBATCH --job-name=magnitude_combined_100Epochs0001
+#SBATCH --job-name=magnitude_combined_40_300_Epochs0001
 #SBATCH --nodes=1
 #SBATCH --time=20:30:00
 #SBATCH --gres=gpu:1
@@ -15,4 +15,5 @@ module add tensorrt/8.6.1.6-cuda12.0-cudnn8.9
 tar -xf $WORK/CombinedDataMag.tar -C $TMPDIR/
 
 cd "$HOME"/altPreprocessing/emrad-analysis || exit
-poetry run python main.py --epochs 100 --learning_rate 0.0001 --image_based False --log False --label_type gaussian --dual_channel False --mag True --combined True
+poetry run python main.py --epochs 40 --learning_rate 0.0001 --image_based False --log False --label_type gaussian --dual_channel False --mag True --combined True
+poetry run python main.py --epochs 300 --learning_rate 0.0001 --image_based False --log False --label_type gaussian --dual_channel False --mag True --combined True
