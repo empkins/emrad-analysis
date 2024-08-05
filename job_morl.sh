8044de4744ba1d5b31df6404a8124f2ea1b7aa41
Simon Meske <simon.h.meske@fau.de>
2024-08-05 14:46:12 +0200

    Test with multunet 3plus

--- job_morl.sh
+++ job_morl.sh
@@ -12,7 +12,7 @@ module load python/3.10-anaconda
 export OUTDATED_IGNORE=1
 export PATH="/home/hpc/iwso/iwso116h/.local/bin:$PATH"
 module add tensorrt/8.6.1.6-cuda12.0-cudnn8.9
-rsync -r $WORK/DataD02 $TMPDIR
+tar -xf $WORK/CombinedData.tar -C $TMPDIR/
 
 cd "$HOME"/altPreprocessing/emrad-analysis || exit
 
