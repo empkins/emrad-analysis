b0d9dea4fb640d5691805ba48414e682b7a200f2
Simon Meske <simon.h.meske@fau.de>
2024-08-05 14:33:20 +0200

    Test with multunet 3plus

--- job_morl.sh
+++ job_morl.sh
@@ -14,6 +14,6 @@ export PATH="/home/hpc/iwso/iwso116h/.local/bin:$PATH"
 module add tensorrt/8.6.1.6-cuda12.0-cudnn8.9
 rsync -r $WORK/DataD02 $TMPDIR
 
-cd "$HOME"/emrad-analysis || exit
+cd "$HOME"/altPreprocessing/emrad-analysis || exit
 
 poetry run python main.py --epochs 40 --learning_rate 0.0001 --image_based False --datasource d02 --log False --label_type gaussian --dual_channel False --wavelet morl
